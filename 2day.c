b1464ed865ec872302b72161e959f76f3893fe90
Iksang_J <wjddlrtkd0110@naver.com>
2024-02-02 14:25:12 +0900

    d

---
+++ 2day.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+float addFloat(float a, float b);
+
+int main()
+{
+    float sum = addFloat(5.5,4.5);
+    printf ("실수의 합 : %.2f\n",sum);
+    return 0;
+}
+
+float addFloat(float a, float b) 
+{
+    return a + b;
+}
